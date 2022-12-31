692007a28558c213cba89380563313743e916331
Rishabh <jharishabh2k3@gmail.com>
2022-12-31 14:42:02 +0530

    update

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/utsname.h>
 
 char website_name[100], website_name2[100];
 char password[100];
@@ -41,21 +40,33 @@ int output() {
     return 0;
 }
 
-int output_file() {
+int output_to_file() {
     FILE *file_ptr = fopen("/home/rishabh/projects/password.txt", "a");
     if (file_ptr == NULL) {
         puts("this file doesnt exist");
-        printf("crate the file");
+        printf("create the file");
     }
-    fprintf(file_ptr, "%s", website_name);
-    fprintf(file_ptr, "%s", password);
+    fprintf(file_ptr, "Site - %s\n", website_name);
+    fprintf(file_ptr, "password - %s\n", password);
+    fclose(file_ptr);
+    return 0;
+}
+
+int scan_from_file() {
+    FILE *file_ptr = fopen("/home/rishabh/projects/password.txt", "r");
+    char buff[255]; // creating char array to store data of file  
+    while (fscanf(file_ptr, "%s", buff) != EOF) {
+        printf("%s\n ", buff);
+    }
+    fclose(file_ptr);
     return 0;
 }
 
 int pass_man() {
     add_data();
     output();
-    output_file();
+    output_to_file();
+    // scan_from_file();
     return 0;
 }
 
@@ -68,8 +79,6 @@ int main() {
         "-------------------------------------------------------------------\n");
     printf("this is a password management system which will store the username "
            "and password");
-    pass_man();
-    system("clear");
     int add_more;
     printf("\nEnter 1 if you want to add more passwords and 2 if you want to "
            "exit - ");
@@ -79,10 +88,15 @@ int main() {
         case 1:
             pass_man();
             break;
-        case 2:
-            printf("\n\tHope you enjoyed it! STAY SAFE");
+        default:
+            printf("\n\tHope this software was useful to you");
+            break;
+        case 3:
+            // scan_from_file();
             break;
         }
+        break;
     }
+    scan_from_file();
     return 0;
 }
