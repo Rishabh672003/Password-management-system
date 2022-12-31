ddea98e9a50d31e185a365e78daa8c9a79970e2c
Rishabh <jharishabh2k3@gmail.com>
2022-12-31 11:20:40 +0530

    update

--- main.c
+++ main.c
@@ -6,10 +6,10 @@ char password[100];
 
 int add_data() {
     printf("\nEnter the site name - ");
-    scanf("%99s", website_name);
-    printf("The password of \t%99s is - ", website_name);
+    scanf("%s", website_name);
+    printf("The password of %s is - ", website_name);
     scanf("\n%99s", password);
-    printf("The site name is %99s and its password is %99s.", website_name,
+    printf("The site name is %s and its password is %s.", website_name,
            password);
     return 0;
 }
