dc72c0514929fb8c6850974263ead2626cb5e18c
Rishabh <jharishabh2k3@gmail.com>
2022-12-31 11:15:29 +0530

    updated stuff

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+char website_name[100], website_name2[100];
+char password[100];
+
+int add_data() {
+    printf("\nEnter the site name - ");
+    scanf("%99s", website_name);
+    printf("The password of \t%99s is - ", website_name);
+    scanf("\n%99s", password);
+    printf("The site name is %99s and its password is %99s.", website_name,
+           password);
+    return 0;
+}
+
 int main() {
     printf(
         "-------------------------------------------------------------------\n");
@@ -8,14 +21,18 @@ int main() {
         "                  PASSWORD MANAGEMENT SYSTEM                       \n");
     printf(
         "-------------------------------------------------------------------\n");
-    char website_name[100];
-    char password[100];
     printf("this is a password management system which will store the username "
            "and password");
-    printf("\nEnter the site name -  ");
-    scanf("%99s", website_name);
-    printf("The password of %99s is - ", website_name);
-    scanf("%99s", password);
-    printf("\n The site name is %99s and its password is %99s.", website_name,
-           password);
+    add_data();
+    int add_more;
+    printf("\nEnter 1 if you want to add more passwords and 2 if you want to exit - ");
+    scanf("%i", &add_more);
+    switch (add_more) {
+    case 1:
+        add_data();
+        break;
+    case 2:
+        printf("Hope you had fun time");
+        break;
+    }
 }
